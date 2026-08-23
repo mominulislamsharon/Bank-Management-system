1dc537c7ab3d0df07db49076b4bb40ca9c3db1c6
mominulislamsharon <mominulislamsharon@gmail.com>
2026-08-24 05:30:19 +0600

    create Error handeling and Middleware

--- backend/app.js
+++ backend/app.js
@@ -9,4 +9,45 @@ const app = express();
 
 app.use(helmet());
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 HOUR
+  max: 10000,
+  message: "Too many request from this IP, please try again in an hour!",
+  standardHeaders: true,
+  legacyHeader: false,
+});
+
+app.use("/api", limiter);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use((req, res, next) => {
+  mongoSanitize.sanitize(req.body);
+  mongoSanitize.sanitize(req.params);
+
+  const queryCopy = { ...req.query };
+  mongoSanitize.sanitize(queryCopy);
+
+  req.querySanitized = queryCopy;
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Just for testing",
+  });
+});
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,14 @@ const DB = process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASSWORD);
 
 mongoose
   .connect(DB)
-  .then(() => console.log("DB Connection Successfull"))
+  .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => console.log(err));
 
 const port = process.env.PORT;
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+  
 });
+
+
