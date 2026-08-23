5a3606228b1afa612132ede78cfc9fca61514a25
mominulislamsharon <mominulislamsharon@gmail.com>
2026-08-24 04:52:42 +0600

    backend setup and mongodb connection

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const DB = process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
