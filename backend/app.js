7e83e17cab02b70987049080f12b66d051d5d6ac
mominulislamsharon <mominulislamsharon@gmail.com>
2026-08-24 05:02:08 +0600

    create file structure and security setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const app = express();
 
-module.exports = app;
+app.use(helmet());
 
+module.exports = app;
